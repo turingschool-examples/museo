9317869131e0ceaa4712be37cf374322fc92292b
Austin Moore <austinchristianmoore@gmail.com>
2022-01-18 15:56:31 -0500

    Fix: Align attributes

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,10 +6,10 @@ class Artist
               :country
 
   def initialize(attributes)
-    @id   = attributes[:id]
-    @name = attributes[:name]
-    @born = attributes[:born]
-    @died = attributes[:died]
+    @id      = attributes[:id]
+    @name    = attributes[:name]
+    @born    = attributes[:born]
+    @died    = attributes[:died]
     @country = attributes[:country]
   end
 
